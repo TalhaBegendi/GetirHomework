0e7f9ce51f803abfedf74ecb43eedef0cd9cff0d
TalhaBegendi <Talhabegendi3@gmail.com>
2022-01-31 17:25:22 +0300

    Update

--- src/test/java/services/SampleTest.java
+++ src/test/java/services/SampleTest.java
@@ -1,9 +1,14 @@
 package services;
 
+import io.qameta.allure.Allure;
 import io.restassured.RestAssured;
+import io.restassured.internal.RequestSpecificationImpl;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.json.JSONObject;
 import java.io.IOException;
+
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -122,5 +127,30 @@ public class SampleTest extends BaseClass {
                 .log().all()
                 .extract().response();
         System.out.println(response.getBody().asString()+" Kayıt Silindi");
+
+
+    }
+
+    @Test
+    public void sample() {
+        String baseUrl = "https://petstore.swagger.io/v2/store/inventory";
+        RequestSpecification restAssuredReq = RestAssured.given()
+                .header("Study","Test")
+                .log()
+                .all(true);
+        Response response = restAssuredReq.get(baseUrl);
+        attachment(restAssuredReq, baseUrl, response);
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+
+
+    public String attachment(RequestSpecification httpRequest, String baseUrl, Response response) {
+        String html = "Url = " + baseUrl + "\n \n" +
+                "Request Headers = " + ((RequestSpecificationImpl) httpRequest).getHeaders() + "\n \n" +
+                "Request Body = " + ((RequestSpecificationImpl) httpRequest).getBody() + "\n \n" +
+                "Response Body = " + response.getBody().asString();
+        Allure.addAttachment("Request Detail", html);
+        return html;
+
     }
 }
