b11bec0a74bec108dff3a5ddf474db4b0648fc94
TalhaBegendi <67201575+TalhaBegendi@users.noreply.github.com>
2022-01-31 18:01:17 +0300

    Update SampleTest.java

--- src/test/java/services/SampleTest.java
+++ src/test/java/services/SampleTest.java
@@ -130,29 +130,6 @@ public class SampleTest extends BaseClass {
 
 
     }
-
-    @Test
-    public void sample() {
-        String baseUrl = "https://petstore.swagger.io/v2/store/inventory";
-        RequestSpecification restAssuredReq = RestAssured.given()
-                .header("Study","Test")
-                .log()
-                .all(true);
-        Response response = restAssuredReq.get(baseUrl);
-        attachment(restAssuredReq, baseUrl, response);
-        Assert.assertEquals(response.getStatusCode(), 200);
-    }
-
-
-    public String attachment(RequestSpecification httpRequest, String baseUrl, Response response) {
-        String html = "Url = " + baseUrl + "\n \n" +
-                "Request Headers = " + ((RequestSpecificationImpl) httpRequest).getHeaders() + "\n \n" +
-                "Request Body = " + ((RequestSpecificationImpl) httpRequest).getBody() + "\n \n" +
-                "Response Body = " + response.getBody().asString();
-        Allure.addAttachment("Request Detail", html);
-        return html;
-
-    }
     
         @Test
     public void sample() {
