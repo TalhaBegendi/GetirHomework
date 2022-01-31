c2935b4e220f9b33a6835b2e38f9a3380dba8278
TalhaBegendi <67201575+TalhaBegendi@users.noreply.github.com>
2022-01-31 21:46:11 +0300

    Update BaseClass.java

--- src/test/java/services/BaseClass.java
+++ src/test/java/services/BaseClass.java
@@ -1,5 +1,7 @@
 package services;
 import com.google.common.io.Resources;
+import io.qameta.allure.Allure;
+import io.restassured.response.Response;
 import org.json.JSONObject;
 import java.io.IOException;
 import java.net.URL;
@@ -14,6 +16,13 @@ public class BaseClass {
         String json = Resources.toString(jsonFile, Charset.defaultCharset());
         JSONObject jsonObject = new JSONObject( json );
         return jsonObject;
+    }
+
+    public String attachment(String baseUrl, Response response) {
+        String html = "Url = " + baseUrl + "\n \n" +
+                "Response Body = " + response.getBody().asString();
+        Allure.addAttachment("Request Detail", html);
+        return html;
     }
 }
 
